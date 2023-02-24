5d0e03797a34e249b5f01907856bd1757f932714
Paul Flammarion <paul@flammarion.eu>
2023-02-24 20:24:33 +0100

    add getters and setters

--- harrypotter/src/main/java/com/isep/harrypotter/Wizard.java
+++ harrypotter/src/main/java/com/isep/harrypotter/Wizard.java
@@ -12,4 +12,43 @@ public class Wizard extends Character{
     public void defend(){
 
     }
+
+    public House getHouse() {
+        return house;
+    }
+    public void setHouse(House house) {
+        this.house = house;
+    }
+
+    public void setPet(Pet pet){
+        this.pet = pet;
+    }
+
+    public Pet getPet() {
+        return pet;
+    }
+
+    public List<Potion> getPotions() {
+        return potions;
+    }
+
+    public void setPotions(List<Potion> potions) {
+        this.potions = potions;
+    }
+
+    public List<Spell> getKnownSpells() {
+        return knownSpells;
+    }
+
+    public void setKnownSpells(List<Spell> knownSpells) {
+        this.knownSpells = knownSpells;
+    }
+
+    public void setWand(Wand wand) {
+        this.wand = wand;
+    }
+
+    public Wand getWand() {
+        return wand;
+    }
 }
