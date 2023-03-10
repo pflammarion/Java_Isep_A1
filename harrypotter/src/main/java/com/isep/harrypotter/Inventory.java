0dcb58fa95919de0bc74ba63971f7ad416f849b4
Paul Flammarion <80546510+pflammarion@users.noreply.github.com>
2023-03-10 10:24:12 +0100

    add inventory (#13)
    Co-authored-by: Paul Flammarion <paul@flammarion.eu>

---
+++ harrypotter/src/main/java/com/isep/harrypotter/Inventory.java
@@ -0,0 +1,13 @@
+package com.isep.harrypotter;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Inventory {
+    private String name;
+    private String description;
+}
