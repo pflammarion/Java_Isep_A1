7282ae9ab774d8ec5cab6e52afbb026c00b22d00
Paul Flammarion <paul@flammarion.eu>
2023-03-20 20:24:51 +0100

    add fight rewards

--- harrypotter/src/main/java/com/isep/harrypotter/model/characters/Boss.java
+++ harrypotter/src/main/java/com/isep/harrypotter/model/characters/Boss.java
@@ -5,8 +5,8 @@ public class Boss extends AbstractEnemy {
     private String specialObject;
     private String specialSpell;
 
-    public Boss(int totalHealth, double currentHealth, int defence, int damage, double accuracy, String name, String specialObject, String specialSpell) {
-        super(totalHealth, currentHealth, defence, damage, accuracy, name);
+    public Boss(int totalHealth, double currentHealth, int defense, int damage, double accuracy, String name, String specialObject, String specialSpell) {
+        super(totalHealth, currentHealth, defense, damage, accuracy, name);
         this.specialObject = specialObject;
         this.specialSpell = specialSpell;
     }
