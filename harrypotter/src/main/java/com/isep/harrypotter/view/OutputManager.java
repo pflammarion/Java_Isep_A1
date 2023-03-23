93db489f1fb8ec4b355281ddef0dc1f9d6b2bddc
Paul Flammarion <paul@flammarion.eu>
2023-03-23 17:33:41 +0100

    update after merge

--- harrypotter/src/main/java/com/isep/harrypotter/view/OutputManager.java
+++ harrypotter/src/main/java/com/isep/harrypotter/view/OutputManager.java
@@ -1,11 +1,5 @@
 package com.isep.harrypotter.view;
 
-import com.isep.harrypotter.model.Potion;
-import com.isep.harrypotter.model.characters.Wizard;
-import com.isep.harrypotter.model.spells.Spell;
-
-import java.io.IOException;
-import java.net.URISyntaxException;
 import java.util.List;
 
 public interface OutputManager {
