042185eeda1ab89eabeef6405032a313f0bccd9a
Paul Flammarion <paul@flammarion.eu>
2023-04-07 11:40:26 +0200

    add java fx welcome layout, and add style.css stylesheet

--- harrypotter/src/main/java/com/isep/harrypotter/Main.java
+++ harrypotter/src/main/java/com/isep/harrypotter/Main.java
@@ -9,13 +9,15 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Game.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/com/isep/harrypotter/scene/hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/com/isep/harrypotter/assets/css/style.css")).toExternalForm());
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
@@ -26,8 +28,8 @@ public class Main extends Application {
         Game game = new Game(new ConsoleParser(System.in), new ConsoleOutput());
 
         //run the console game
-        game.play();
-        //launch();
+        //game.play();
+        launch();
     }
 
 }
--- harrypotter/src/main/java/com/isep/harrypotter/controller/HelloController.java
+++ harrypotter/src/main/java/com/isep/harrypotter/controller/HelloController.java
@@ -2,6 +2,9 @@ package com.isep.harrypotter.controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+import java.net.URL;
 
 public class HelloController {
     @FXML
@@ -10,5 +13,15 @@ public class HelloController {
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    @FXML
+    protected void onBeginButtonClick(){
+
+    }
+
+    @FXML
+    protected void onQuitButtonClick() {
+        System.exit(0);
     }
 }
